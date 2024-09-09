955e5d98bcf176df44dc8991c9494a8fb7201d5a
Felipe Mariotti <felipe.mtt95@gmail.com>
2024-09-09 06:13:01 -0300

    feat: Implement handleFields

--- zap.go
+++ zap.go
@@ -13,7 +13,7 @@ func New(logger *zap.Logger) stdlog.Logger[zap.Field] {
 	return &zLogger{logger}
 }
 
-func (z *zLogger) Named(name string) stdlog.Logger[zap.Field] {
+func (z *zLogger) Named(name string) stdlog.Logger {
 	return New(z.Logger.Named(name))
 }
 
@@ -46,4 +46,20 @@ func (z *zLogger) Fatal(msg string, fields ...zap.Field) {
 
 func (z *zLogger) FatalError(err error, msg string, fields ...zap.Field) {
 	z.Logger.Fatal(msg, append(fields, zap.Error(err))...)
+}
+
+func handleFields(level stdlog.Level, kvs []any) []zap.Field {
+	if len(kvs)%2 != 0 {
+		panic(fmt.Errorf("uneven keys and values passed to %s", level.String()))
+	}
+
+	fields := make([]zap.Field, 0, len(kvs)/2)
+
+	for i := 0; i < len(kvs)-1; i += 2 {
+		k := fmt.Sprintf("%v", kvs[i])
+		v := fmt.Sprintf("%v", kvs[i+1])
+		fields = append(fields, zap.Any(k, v))
+	}
+
+	return fields
 }
